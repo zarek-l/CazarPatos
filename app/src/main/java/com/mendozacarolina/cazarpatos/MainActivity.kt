cb7ad6b6336419d0ac5ca2b9bcfadd91ef29948f
Carolina Mendoza <carolina.mendoza@epn.edu.ec>
2022-02-13 19:32:31 -0500

    Lab_09

--- app/src/main/java/com/mendozacarolina/cazarpatos/MainActivity.kt
+++ app/src/main/java/com/mendozacarolina/cazarpatos/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mendozacarolina.cazarpatos
 
+import android.content.Intent
 import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -8,8 +10,16 @@ import android.widget.TextView
 import android.os.Handler
 import java.util.*
 import android.os.CountDownTimer
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var textViewContador: TextView
     lateinit var textViewTiempo: TextView
     lateinit var imageViewPato: ImageView
+    lateinit var mAdView : AdView
     var contador = 0
     var anchoPantalla = 0
     var alturaPantalla = 0
@@ -34,9 +45,16 @@ class MainActivity : AppCompatActivity() {
         textViewTiempo = findViewById(R.id.textViewTiempo)
         imageViewPato = findViewById(R.id.imageViewPato)
 
+        //inicializacion publicidad
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
         //Obtener el usuario de pantalla login
         val extras = intent.extras ?: return
-        val usuario = extras.getString(EXTRA_LOGIN) ?:"Unknown"
+        var usuario = extras.getString(EXTRA_LOGIN) ?:"Unknown"
+        usuario = usuario.substringBefore("@")
         textViewUsuario.setText(usuario)
 
         //Determina el ancho y largo de pantalla
@@ -58,7 +76,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun inicializarPantalla() {
-        //Obtener el tamaño de pantalla
+        // 1. Obtenemos el tamaño de la pantalla del dispositivo
         val display = this.resources.displayMetrics
         anchoPantalla = display.widthPixels
         alturaPantalla = display.heightPixels
@@ -68,11 +86,11 @@ class MainActivity : AppCompatActivity() {
         val min = imageViewPato.getWidth()/2
         val maximoX = anchoPantalla - imageViewPato.getWidth()
         val maximoY = alturaPantalla - imageViewPato.getHeight()
-        //Genera 2 números aleatorios para la coordenadas x  y
+        // Generamos 2 números aleatorios, para la coordenadas x , y
         val randomX = Random().nextInt(maximoX - min + 1)
         val randomY = Random().nextInt(maximoY - min + 1)
 
-        // Utiliza números aleatorios para mover el pato a esa posición
+        // Utilizamos los números aleatorios para mover el pato a esa nueva posición
         imageViewPato.setX(randomX.toFloat())
         imageViewPato.setY(randomY.toFloat())
     }
@@ -87,9 +105,11 @@ class MainActivity : AppCompatActivity() {
                 textViewTiempo.setText("0s")
                 gameOver = true
                 mostrarDialogoGameOver()
+                val nombreJugador = textViewUsuario.text.toString()
+                val patosCazados = textViewContador.text.toString()
+                procesarPuntajePatosCazados(nombreJugador, patosCazados.toInt())
             }
         }.start()
-
     }
     private fun mostrarDialogoGameOver() {
         val builder = AlertDialog.Builder(this)
@@ -98,11 +118,7 @@ class MainActivity : AppCompatActivity() {
             .setTitle("Fin del juego")
             .setPositiveButton("Reiniciar",
                 { _, _ ->
-                    contador = 0
-                    gameOver = false
-                    textViewContador.setText(contador.toString())
-                    moverPato()
-                    inicializarCuentaRegresiva()
+                    reiniciarJuego()
                 })
             .setNegativeButton("Cerrar",
                 { _, _ ->
@@ -111,10 +127,96 @@ class MainActivity : AppCompatActivity() {
         builder.create().show()
     }
 
+    fun reiniciarJuego(){
+        contador = 0
+        gameOver = false
+        textViewContador.setText(contador.toString())
+        moverPato()
+        inicializarCuentaRegresiva()
+    }
+    fun jugarOnline(){
+        var intentWeb = Intent()
+        intentWeb.action = Intent.ACTION_VIEW
+        intentWeb.data = Uri.parse("https://duckhuntjs.com/")
+        startActivity(intentWeb)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_nuevo_juego -> {
+                reiniciarJuego()
+                true
+            }
+            R.id.action_jugar_online -> {
+                jugarOnline()
+                true
+            }
+            R.id.action_ranking -> {
+                val intent = Intent(this, RankingActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
-
+    fun procesarPuntajePatosCazados(nombreJugador:String, patosCazados:Int){
+        val jugador = Jugador(nombreJugador,patosCazados)
+        //Trata de obtener id del documento del ranking específico,
+        // si lo obtiene lo actualiza, caso contrario lo crea
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .whereEqualTo("usuario", jugador.usuario)
+            .get()
+            .addOnSuccessListener { documents ->
+                if(documents!= null &&
+                    documents.documents != null &&
+                    documents.documents.count()>0
+                ){
+                    val idDocumento = documents.documents.get(0).id
+                    actualizarPuntajeJugador(idDocumento, jugador)
+                }
+                else{
+                    ingresarPuntajeJugador(jugador)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents", exception)
+                Toast.makeText(this, "Error al obtener datos de jugador", Toast.LENGTH_LONG).show()
+            }
+    }
+    fun ingresarPuntajeJugador(jugador:Jugador){
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .add(jugador)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(this,"Puntaje usuario ingresado exitosamente", Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error adding document", exception)
+                Toast.makeText(this,"Error al ingresar el puntaje", Toast.LENGTH_LONG).show()
+            }
+    }
+    fun actualizarPuntajeJugador(idDocumento:String, jugador:Jugador){
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .document(idDocumento)
+            //.update(contactoHashMap)
+            .set(jugador) //otra forma de actualizar
+            .addOnSuccessListener {
+                Toast.makeText(this,"Puntaje de usuario actualizado exitosamente", Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error updating document", exception)
+                Toast.makeText(this,"Error al actualizar el puntaje" , Toast.LENGTH_LONG).show()
+            }
+    }
 
 }
+
 
 
 
--- app/src/main/java/com/mendozacarolina/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/mendozacarolina/cazarpatos/RankingActivity.kt
@@ -2,18 +2,23 @@ package com.mendozacarolina.cazarpatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.mendozacarolina.cazarpatos.database.RankingPlayerDBHelper
 
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ranking)
-
-        OperacionesSqLite()
-        GrabarRankingSQLite()
-        LeerRankingsSQLite()
+        consultarPuntajeJugadores()
+        //OperacionesSqLite()
+        //GrabarRankingSQLite()
+        //LeerRankingsSQLite()
     }
     fun OperacionesSqLite(){
         RankingPlayerDBHelper(this).deleteAllRanking()
@@ -42,6 +47,32 @@ class RankingActivity : AppCompatActivity() {
         recyclerViewRanking.layoutManager = LinearLayoutManager(this)
         recyclerViewRanking.adapter = RankingAdapter(jugadoresSQLite)
         recyclerViewRanking.setHasFixedSize(true)
+    }
+
+    fun consultarPuntajeJugadores() {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .orderBy("patosCazados", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(EXTRA_LOGIN, "Success getting documents")
+                var jugadores = ArrayList<Jugador>()
+                for (document in result) {
+                    val jugador = document.toObject(Jugador::class.java)
+                    //val jugador = document.toObject<Jugador>()
+                    jugadores.add(jugador)
+                }
+                //Poblar en RecyclerView información usando mi adaptador
+                val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+                recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+                recyclerViewRanking.adapter = RankingAdapter(jugadores);
+                recyclerViewRanking.setHasFixedSize(true);
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents.", exception)
+                Toast.makeText(this, "Error al obtener datos de jugadores", Toast.LENGTH_LONG)
+                    .show()
+            }
     }
 }
 
